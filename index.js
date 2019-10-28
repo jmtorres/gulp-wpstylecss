46a22939ce6bbf30de4945b6df0622b7090bd78b
Eirik Refsdal <eirikref@gmail.com>
2019-10-28 14:13:22 +0100

    Fix indentation
    - Clean up code:
        - Fix indentation (two spaces is unreadable)
        - Format code better
        - Don’t use single quotes
    - Update deps
    - Fix tests
    - Change order of fields to match official WordPress docs
    - Stop using deprecated libs, and update code, tests, and npm scripts accordingly

--- index.js
+++ index.js
@@ -1,85 +1,93 @@
-var fs = require('fs');
-var Readable = require('stream').Readable;
-var gutil = require('gulp-util');
-var _ = require('lodash');
-var _s = require('underscore.string');
+var fs          = require("fs");
+var Readable    = require("stream").Readable;
+var PluginError = require("plugin-error");
+var Vinyl       = require("vinyl");
+var _           = require("lodash");
+var _s          = require("underscore.string");
+var pkg         = JSON.parse(fs.readFileSync("package.json", {encoding: "utf8"}));
 
-var pkg = JSON.parse(fs.readFileSync('package.json', {encoding: 'utf8'}));
+module.exports = function (options)
+{
+    "use strict";
 
-module.exports = function (options) {
-  'use strict';
+    options = _.defaults(options || {}, {
+        path:        "style.css",
+        name:        _s.titleize(_s.humanize(pkg.name)),
+        description: pkg.description,
+        version:     pkg.version,
+        uri:         pkg.homepage,
+        tags:        pkg.keywords,
+        author:      pkg.author.name,
+        authorUri:   pkg.author.url,
+        license:     pkg.license,
+        licenseUri:  null
+    });
 
-  options = _.defaults(options || {}, {
-    path: 'style.css',
-    name: _s.titleize(_s.humanize(pkg.name)),
-    description: pkg.description,
-    version: pkg.version,
-    uri: pkg.homepage,
-    tags: pkg.keywords,
-    author: pkg.author.name,
-    authorUri: pkg.author.url,
-    license: pkg.license,
-    licenseUri: null
-  });
+    if (!options.path) {
+        throw new PluginError("gulp-wpstylecss", "`path` is required.");
+    }
 
-  if (!options.path) {
-    throw new gutil.PluginError('gulp-wpstylecss', '`path` is required!');
-  }
+    if (!options.name) {
+        throw new PluginError("gulp-wpstylecss", "`name` is required.");
+    }
 
-  if (!options.name) {
-    throw new gutil.PluginError('gulp-wpstylecss', '`name` is required!');
-  }
+    /*
+      Theme Name:     <%= options.name %>
+      Theme URI:      <%= options.homepage %>
+      Author:         <%= options.author.name %>
+      Author URI:     <%= options.author.url %>
+      Description:    <%= options.description %>
+      Version:        <%= options.version %>
+      License:        <%= options.license %>
+      License URI:    <%= options.licenseUri %>
+      Tags:           <%= options.keywords %>
+    */
+    var contents = "/*\n";
 
-  /*
-  Theme Name:     <%= options.name %>
-  Description:    <%= options.description %>
-  Version:        <%= options.version %>
-  Theme URI:      <%= options.homepage %>
-  Tags:           <%= options.keywords %>
-  Author:         <%= options.author.name %>
-  Author URI:     <%= options.author.url %>
-  License:        <%= options.license %>
-  License URI:    <%= options.licenseUri %>
-  */
-  var contents = '/*\n';
+    contents += "Theme Name:     " + options.name + "\n";
 
-  contents += 'Theme Name:     ' + options.name + '\n';
+    if (options.uri) {
+        contents += "Theme URI:      " + options.uri + "\n";
+    }
 
-  if (options.description) {
-    contents += 'Description:    ' + options.description + '\n';
-  }
-  if (options.version) {
-    contents += 'Version:        ' + options.version + '\n';
-  }
-  if (options.uri) {
-    contents += 'Theme URI:      ' + options.uri + '\n';
-  }
-  if (options.tags) {
-    contents += 'Tags:           ' + options.tags + '\n';
-  }
-  if (options.author) {
-    contents += 'Author:         ' + options.author + '\n';
-  }
-  if (options.authorUri) {
-    contents += 'Author URI:     ' + options.authorUri + '\n';
-  }
-  if (options.license) {
-    contents += 'License:        ' + options.license + '\n';
-  }
-  if (options.licenseUri) {
-    contents += 'License URI:    ' + options.licenseUri + '\n';
-  }
+    if (options.author) {
+        contents += "Author:         " + options.author + "\n";
+    }
 
-  contents += '*/\n';
+    if (options.authorUri) {
+        contents += "Author URI:     " + options.authorUri + "\n";
+    }
 
-  var stream = new Readable({objectMode: true});
+    if (options.description) {
+        contents += "Description:    " + options.description + "\n";
+    }
 
-  stream.push(new gutil.File({
-    path: options.path,
-    contents: new Buffer(contents, 'utf8')
-  }));
+    if (options.version) {
+        contents += "Version:        " + options.version + "\n";
+    }
 
-  stream.push(null);
+    if (options.license) {
+        contents += "License:        " + options.license + "\n";
+    }
 
-  return stream;
+    if (options.licenseUri) {
+        contents += "License URI:    " + options.licenseUri + "\n";
+    }
+
+    if (options.tags) {
+        contents += "Tags:           " + options.tags + "\n";
+    }
+
+    contents += "*/\n";
+
+    var stream = new Readable({objectMode: true});
+
+    stream.push(new Vinyl({
+        path: options.path,
+        contents: new Buffer.from(contents, "utf8")
+    }));
+
+    stream.push(null);
+
+    return stream;
 };
--- test/main.js
+++ test/main.js
@@ -1,82 +1,82 @@
 /*global describe, it*/
-'use strict';
+"use strict";
 
-var fs = require('fs'),
-should = require('should');
+var fs         = require("fs");
+var should     = require("should");
+var Vinyl      = require("vinyl");
+var wpstylecss = require("../");
 
-require('mocha');
+require("mocha");
 
-delete require.cache[require.resolve('../')];
+delete require.cache[require.resolve("../")];
 
-var gutil = require('gulp-util'),
-wpstylecss = require('../');
 
-describe('gulp-wpstylecss', function () {
-  it('should produce expected file from package.json', function (done) {
-    var expectedFile = new gutil.File({
-      path: 'style.css',
-      cwd: null,
-      base: null,
-      contents: fs.readFileSync('test/expected/default.style.css')
+describe("gulp-wpstylecss", function () {
+    it("should produce expected file from package.json", function (done) {
+        var expectedFile = new Vinyl({
+            path: "style.css",
+            cwd: null,
+            base: null,
+            contents: fs.readFileSync("test/expected/default.style.css")
+        });
+
+        var stream = wpstylecss();
+
+        stream.on("error", function (err) {
+            should.exist(err);
+            done(err);
+        });
+
+        stream.on("data", function (newFile) {
+            should.exist(newFile);
+            should.exist(newFile.contents);
+
+            String(newFile.path).should.equal(String(expectedFile.path));
+            String(newFile.cwd).should.equal(String(expectedFile.cwd));
+            String(newFile.base).should.equal(String(expectedFile.base));
+            String(newFile.contents).should.equal(String(expectedFile.contents));
+
+            done();
+        });
     });
 
-    var stream = wpstylecss();
+    it("should produce expected file via stream", function (done) {
+        var expectedFile = new Vinyl({
+            path: "PATH",
+            cwd: null,
+            base: null,
+            contents: fs.readFileSync("test/expected/custom.style.css")
+        });
 
-    stream.on('error', function (err) {
-      should.exist(err);
-      done(err);
+        var stream = wpstylecss({
+            path: "PATH",
+            name: "THEME_NAME",
+            description: "DESCRIPTION",
+            version: "VERSION",
+            uri: "THEME_URI",
+            tags: ["TAG1", "TAG2", "TAG3"],
+            author: "AUTHOR",
+            authorUri: "AUTHOR_URI",
+            license: "LICENSE",
+            licenseUri: "LICENSE_URI"
+        });
+
+        stream.on("error", function (err) {
+            should.exist(err);
+            done(err);
+        });
+
+        stream.on("data", function (newFile) {
+            should.exist(newFile);
+            should.exist(newFile.contents);
+
+            String(newFile.path).should.equal(String(expectedFile.path));
+            String(newFile.cwd).should.equal(String(expectedFile.cwd));
+            String(newFile.base).should.equal(String(expectedFile.base));
+            String(newFile.contents).should.equal(String(expectedFile.contents));
+
+            done();
+        });
     });
-
-    stream.on('data', function (newFile) {
-      should.exist(newFile);
-      should.exist(newFile.contents);
-
-      String(newFile.path).should.equal(String(expectedFile.path));
-      String(newFile.cwd).should.equal(String(expectedFile.cwd));
-      String(newFile.base).should.equal(String(expectedFile.base));
-      String(newFile.contents).should.equal(String(expectedFile.contents));
-
-      done();
-    });
-  });
-
-  it('should produce expected file via stream', function (done) {
-    var expectedFile = new gutil.File({
-      path: 'PATH',
-      cwd: null,
-      base: null,
-      contents: fs.readFileSync('test/expected/custom.style.css')
-    });
-
-    var stream = wpstylecss({
-      path: 'PATH',
-      name: 'THEME_NAME',
-      description: 'DESCRIPTION',
-      version: 'VERSION',
-      uri: 'THEME_URI',
-      tags: ['TAG1', 'TAG2', 'TAG3'],
-      author: 'AUTHOR',
-      authorUri: 'AUTHOR_URI',
-      license: 'LICENSE',
-      licenseUri: 'LICENSE_URI'
-    });
-
-    stream.on('error', function (err) {
-      should.exist(err);
-      done(err);
-    });
-
-    stream.on('data', function (newFile) {
-      should.exist(newFile);
-      should.exist(newFile.contents);
-
-      String(newFile.path).should.equal(String(expectedFile.path));
-      String(newFile.cwd).should.equal(String(expectedFile.cwd));
-      String(newFile.base).should.equal(String(expectedFile.base));
-      String(newFile.contents).should.equal(String(expectedFile.contents));
-
-      done();
-    });
-  });
 
 });
